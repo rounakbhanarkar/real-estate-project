f5a7a5fc43bb61a7dd612a9e2b7ebea4a9df0d47
Rounak Bhanarkar <rounakbhanarkar10@gmail.com>
2024-03-28 17:49:34 +0530

    // first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,25 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel,MdAnalytics } from "react-icons/md";
+
+const data=[
+    {
+        icon:<HiShieldCheck/>,
+        heading:"Best interest rates on the market",
+        detail:
+        "Exercitation in fugiat est udt ad ea cupidatat ut in cupidatat occaecat ut occaecat consequent."
+    },
+    {
+        icon:<MdCancel/>,
+        heading:"Prevent unstable prices",
+        detail:
+        "Exercitation in fugiat est udt ad ea cupidatat ut in cupidatat occaecat ut occaecat consequent."
+    },
+    {
+        icon:<MdAnalytics/>,
+        heading:"Best prices on the market",
+        detail:
+        "Exercitation in fugiat est udt ad ea cupidatat ut in cupidatat occaecat ut occaecat consequent."
+    },
+];
+
+export default data;
